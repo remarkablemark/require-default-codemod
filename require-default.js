c329194328e653b644b7d399a0154bbba41b6f2d
Mark <mark@remarkablemark.org>
2020-06-27 17:51:36 -0400

    fix(require-default): filter and exclude `require().default`
    To prevent the possibility of the following:
    
    ```js
    require('a').default.default;
    ```

--- require-default.js
+++ require-default.js
@@ -13,15 +13,24 @@
 module.exports = function (fileInfo, api) {
   var j = api.jscodeshift;
 
-  return j(fileInfo.source)
-    .find(j.CallExpression, {
-      callee: {
-        type: 'Identifier',
-        name: 'require'
-      }
-    })
-    .replaceWith(function (path) {
-      return j.memberExpression(path.value, j.identifier('default'));
-    })
-    .toSource();
+  return (
+    j(fileInfo.source)
+      // find all `require()`
+      .find(j.CallExpression, {
+        callee: {
+          type: 'Identifier',
+          name: 'require'
+        }
+      })
+      // exclude `require()` with `.default`
+      .filter(function (path) {
+        var property = path.parent.node.property;
+        return property ? property.name !== 'default' : true;
+      })
+      // append `.default` to `require()`
+      .replaceWith(function (path) {
+        return j.memberExpression(path.value, j.identifier('default'));
+      })
+      .toSource()
+  );
 };
